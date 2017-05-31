14d3a4e965fdcc556495f60c4217c5b0c7d078c7
shawn sandy <shawnsandy04@gmail.com>
2017-05-31 14:03:15 -0400

    FIX - Modify includes on provider to point to the correct path

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -1,5 +1,5 @@
-@extends(':package_name::shared.app')
+@extends(':package_name::layouts.layout')
 @section('title', ':package_name')
 @section('content')
-  @include(':package_name::shared.index')
+  @include(':package_name::index')
 @endsection
