0877d0f488a2252e404847dd52fb2940d23015ad
shawn sandy <shawnsandy04@gmail.com>
2017-09-01 15:40:14 -0400

    update - add migratiuons commands to boot function

--- src/ServicesProvider.php
+++ src/ServicesProvider.php
@@ -4,137 +4,157 @@ namespace :namespace;
 
 use Illuminate\Support\ServiceProvider;
 
+
 /**
- * Class Provider
+* Class Provider
  * @package :namespace
  */
 class :providerServicesProvider extends ServiceProvider
 {
-    /**
-     * Perform post-registration booting of services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
 
-
-        /**
-         * Package views
-         */
-        $this->loadViewsFrom(__DIR__ . '/resources/views', ':package_name');
-        $this->publishes(
-            [
-                __DIR__ . '/resources/views' => resource_path('views/vendor/:package_name'),
-            ], ':package_name-views'
-        );
-
-        /**
-         * Package assets
-         */
-        $this->publishes(
-            [
-                __DIR__.'/resources/assets/js/' => public_path('assets/:package_name/js/'),
-                __DIR__.'/public/assets/' => public_path('assets/')
-            ], ':package_name-assets'
-        );
-        );
-
-        /**
-         * Package resources to resources
-         */
-        $this->publishes(
-            [
-                __DIR__.'/resources/assets/' => resource_path('assets/:package_name/'),
-            ], ':package_name-resources'
-        );
-
-        /**
-         * Package config
-         */
-        $this->publishes(
-            [__DIR__ . '/config/config.php' => config_path(':package_name.php')],
-            ':package_name-config'
-        );
-
-        if (!$this->app->runningInConsole()) :
-            include_once __DIR__ . '/Helpers/helper.php';
-        endif;
-
-        $this->loadProviders();
-        $this->loadAliases();
+	/**
+	* Perform post-registration booting of services.
+	     *
+	     * @return void
+	     */
+	    public function boot()
+	    {
 
 
 
-    }
-
-    /**
-     * Register any package services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-
-       $this->mergeConfigFrom(
-            __DIR__ . '/config/config.php', ':package_name'
-        );
-
-        $this->app->bind(
-        ':provider', function () {
-        return new :provider();
-        }
-        );
-    }
+		/**
+		* Package views
+		         */
+		        $this->loadViewsFrom(__DIR__ . '/resources/views', ':package_name');
+		$this->publishes(
+		            [
+		                __DIR__ . '/resources/views' => resource_path('views/vendor/:package_name'),
+		            ], ':package_name-views'
+		        );
 
 
-        public function loadProviders()
-        {
-
-            /*
-             * load service providers
-             */
-
-            // $this->app->register(\Collective\Html\HtmlServiceProvider::class);
-
-
-
-
-            /*
-             * Dev resources
-             */
-
-            if ($this->app->environment() !== "production") {
+		/**
+		* Package assets
+		         */
+		        $this->publishes(
+		            [
+		                __DIR__.'/resources/assets/js/' => public_path('assets/:package_name/js/'),
+		                __DIR__.'/public/assets/' => public_path('assets/')
+		            ], ':package_name-assets'
+		        );
+		);
 
 
-            }
+		/**
+		* Package resources to resources
+		         */
+		        $this->publishes(
+		            [
+		                __DIR__.'/resources/assets/' => resource_path('assets/:package_name/'),
+		            ], ':package_name-resources'
+		        );
 
 
-        }
+		/**
+		* Package config
+		         */
+		        $this->publishes(
+		            [__DIR__ . '/config/config.php' => config_path(':package_name.php')],
+		            ':package_name-config'
+		        );
 
-        public function loadAliases() {
-
-
-            /*
-             * Load aliases / facades
-             */
-            $aliases = \Illuminate\Foundation\AliasLoader::getInstance();
-
-           // $aliases->alias("Html", \Collective\Html\HtmlFacade::class);
-
-
-            /*
-           * Dev resources
-           */
-
-            if ($this->app->environment() !== "production") {
-
-                //$aliases->alias('Debugbar', 'Barryvdh\Debugbar\Facade');
-
-            }
+		$this->publishes([
+				            __DIR__ . '/migrations/' => database_path('migrations')
+				        ], ':package_name-migrations');
 
 
-        }
+		$this->loadMigrationsFrom(__DIR__ . '/migrations');
+
+
+
+		if (!$this->app->runningInConsole()) :
+		            include_once __DIR__ . '/Helpers/helper.php';
+		endif;
+
+		$this->loadProviders();
+		$this->loadAliases();
+
+
+
+	}
+
+
+	/**
+	* Register any package services.
+	     *
+	     * @return void
+	     */
+	    public function register()
+	    {
+
+		$this->mergeConfigFrom(
+		            __DIR__ . '/config/config.php', ':package_name'
+		        );
+
+		$this->app->bind(
+		        ':provider', function () {
+			return new :provider();
+		}
+		);
+	}
+
+
+	public function loadProviders()
+	        {
+
+
+		/*
+		* load service providers
+		             */
+
+		// 		$this->app->register(\Collective\Html\HtmlServiceProvider::class);
+
+
+
+
+
+		/*
+		* Dev resources
+		             */
+
+		if ($this->app->environment() !== "production") {
+
+
+		}
+
+
+	}
+
+	public function loadAliases() {
+
+
+
+		/*
+		* Load aliases / facades
+		             */
+		            $aliases = \Illuminate\Foundation\AliasLoader::getInstance();
+
+		// 		$aliases->alias("Html", \Collective\Html\HtmlFacade::class);
+
+
+
+		/*
+		* Dev resources
+		           */
+
+		if ($this->app->environment() !== "production") {
+
+			//$			aliases->alias('Debugbar', 'Barryvdh\Debugbar\Facade');
+
+		}
+
+
+	}
 
 
 }
